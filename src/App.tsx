0fbe2dd4d20b305f76f0af32966165926cd67209
Charles Li <96878550+41CharlesLi@users.noreply.github.com>
2022-10-27 15:21:52 -0400

    rough pseudo code

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 import { useState } from "react";
 
+// input boxes to search artist name and price range (maybe a drop down)
+// component to render results with a component to render each card
+// add shows to a list...and there's a submit button
+// on submit, send results to firebase
+// there's a public list page that shows everyone's wishlists
+// error handle results + close off api calls if user navigates away from page
+// error pages and path protection
+// ability to log in anon or google
+
 function App() {
     return <div className="App"></div>;
 }
